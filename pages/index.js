c5ec4efbfbe9f8f95eab4cb5126f376c6c9065ed
Sarah Ransohoff <sranso@gmail.com>
2023-02-27 15:06:56 -0500

    Fix font link

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
           key="fonts-google-static"
         />
         <link
-          href="https://fonts.googleapis.com/css2?Noto+Serif+JP:wght@400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@400;700&display=swap"
           rel="stylesheet"
           key="fonts-google-noto-serif"
         ></link>

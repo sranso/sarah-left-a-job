0311c38048052d0a34195d9f21ede68bde87aad8
Sarah Ransohoff <sranso@gmail.com>
2023-04-14 08:14:48 -0400

    update

--- components/data/PaceOfInterviews.js
+++ components/data/PaceOfInterviews.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -9,9 +9,9 @@ import {
   Tooltip,
   Filler,
   Legend,
-} from "chart.js";
-import { Line } from "react-chartjs-2";
-import styles from "../../styles/Data.module.css";
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import styles from '../../styles/Data.module.css';
 
 ChartJS.register(
   CategoryScale,
@@ -26,48 +26,41 @@ ChartJS.register(
 
 const data = {
   labels: [
-    "September",
-    "October",
-    "November",
-    "December",
-    "January",
-    "February",
+    'September',
+    'October',
+    'November',
+    'December',
+    'January',
+    'February',
   ],
   datasets: [
     {
       fill: true,
-      label: "Job interviews",
+      label: 'Job interviews',
       data: [0, 3, 5, 20, 20, 8],
-      borderColor: "rgba(53, 162, 235, 0.6)",
-      backgroundColor: "rgba(53, 162, 235, 0.25)",
+      borderColor: 'rgba(53, 162, 235, 0.6)',
+      backgroundColor: 'rgba(53, 162, 235, 0.25)',
     },
     {
       fill: true,
-      label: "Job rejections",
+      label: 'Job rejections',
       data: [0, 6, 4, 3, 2, 1],
-      borderColor: "rgba(80, 81, 242, 0.6)",
-      backgroundColor: "rgba(80, 81, 242, 0.25)",
+      borderColor: 'rgba(80, 81, 242, 0.6)',
+      backgroundColor: 'rgba(80, 81, 242, 0.25)',
     },
-    // {
-    //   fill: true,
-    //   label: "Art acceptances",
-    //   data: [0, 2, 0, 1, 2, 1],
-    //   borderColor: "rgba(4, 109, 145, 0.6)",
-    //   backgroundColor: "rgba(4, 109, 145, 0.4)",
-    // },
     {
       fill: true,
-      label: "Art rejections",
+      label: 'Art rejections',
       data: [2, 2, 3, 1, 1, 1],
-      borderColor: "rgba(4, 199, 145, 0.6)",
-      backgroundColor: "rgba(4, 199, 145, 0.25)",
+      borderColor: 'rgba(4, 199, 145, 0.6)',
+      backgroundColor: 'rgba(4, 199, 145, 0.25)',
     },
     {
       fill: true,
-      label: "Romantic rejections",
-      data: [1, 0, 0, 0, 0, 0, 0],
-      borderColor: "rgba(255, 99, 132, 0.6)",
-      backgroundColor: "rgba(255, 99, 132, 0.25)",
+      label: 'Romantic rejections',
+      data: [1, 0, 0, 0, 1, 0, 0],
+      borderColor: 'rgba(255, 99, 132, 0.6)',
+      backgroundColor: 'rgba(255, 99, 132, 0.25)',
     },
   ],
 };
